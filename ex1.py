2cfcc69a62a310b3c014c820e9203875d25d02a8
taufik.jr@gmail.com <taufik.jr@gmail.com>
2026-02-07 11:20:23 +0800

    first commit

---
+++ ex1.py
@@ -0,0 +1,7 @@
+def welcome(fname, lname):
+    print('Welcome To Pytho Function Mr.\\Ms. '+fname+' '+lname)
+
+
+first_name = input('Enter First Name:  ')
+last_name = input('Enter Last Name:  ')
+welcome(first_name, last_name)
